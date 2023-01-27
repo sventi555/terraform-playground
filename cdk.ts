dea85469e2963c72320dcae76c14333f70a29abf
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2023-01-27 12:58:53 -0500

    feat: add domain mapping

--- cdk.ts
+++ cdk.ts
@@ -1,40 +1,48 @@
 import "dotenv/config";
 
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import {
+  App,
+  MapTerraformIterator,
+  TerraformIterator,
+  TerraformStack,
+} from "cdktf";
 import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
 import { Image } from "@cdktf/provider-docker/lib/image";
 import { RegistryImage } from "@cdktf/provider-docker/lib/registry-image";
 import { ArtifactRegistryRepository } from "@cdktf/provider-google/lib/artifact-registry-repository";
+import { CloudRunDomainMapping } from "@cdktf/provider-google/lib/cloud-run-domain-mapping";
 import { CloudRunService } from "@cdktf/provider-google/lib/cloud-run-service";
-import { CloudRunServiceIamBinding } from "@cdktf/provider-google/lib/cloud-run-service-iam-binding";
 import { CloudRunServiceIamPolicy } from "@cdktf/provider-google/lib/cloud-run-service-iam-policy";
+import { DataGoogleIamPolicy } from "@cdktf/provider-google/lib/data-google-iam-policy";
+import { DnsRecordSet } from "@cdktf/provider-google/lib/dns-record-set";
 import { GoogleProvider } from "@cdktf/provider-google/lib/provider";
 import { readFileSync } from "fs";
-
-import { DataGoogleIamPolicy } from "@cdktf/provider-google/lib/data-google-iam-policy";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    const region = "northamerica-northeast2";
+    const US_E1 = "us-east1";
+    const NA_NE2 = "northamerica-northeast2";
 
-    const googleProvider = new GoogleProvider(this, "google", {
+    const project = "terraform-playground-375515";
+
+    new GoogleProvider(this, "google", {
       credentials: readFileSync(
         process.env.GCP_CREDENTIALS_PATH || "",
         "utf-8"
       ),
-      project: "terraform-playground-375515",
-      region,
-      zone: "northamerica-northeast2-a",
+      project,
+      region: NA_NE2,
+      zone: `${NA_NE2}-a`,
     });
 
     const artifactRegistry = new ArtifactRegistryRepository(
       this,
       "artifactRegistry",
       {
-        location: region,
+        location: NA_NE2,
         format: "DOCKER",
         repositoryId: "terraform-playground",
       }
@@ -43,7 +51,7 @@ class MyStack extends TerraformStack {
     new DockerProvider(this, "docker", {
       registryAuth: [
         {
-          address: `${region}-docker.pkg.dev`,
+          address: `${artifactRegistry.location}-docker.pkg.dev`,
           username: "_json_key_base64",
           password: readFileSync(
             process.env.GCP_CREDENTIALS_PATH || "",
@@ -54,19 +62,19 @@ class MyStack extends TerraformStack {
     });
 
     const image = new Image(this, "dockerImage", {
-      name: `${region}-docker.pkg.dev/${artifactRegistry.project}/${artifactRegistry.repositoryId}/tf-playground:1.0.0`,
+      name: `${artifactRegistry.location}-docker.pkg.dev/${artifactRegistry.project}/${artifactRegistry.repositoryId}/tf-playground:1.0.0`,
       buildAttribute: {
         context: __dirname,
         platform: "linux/amd64",
       },
     });
 
-    new RegistryImage(this, "registryImage", {
+    const registryImage = new RegistryImage(this, "registryImage", {
       name: image.name,
     });
 
     const runService = new CloudRunService(this, "runService", {
-      location: region,
+      location: US_E1, // to support domain mapping
       name: "tf-playground",
 
       template: {
@@ -74,6 +82,8 @@ class MyStack extends TerraformStack {
           containers: [{ image: image.name }],
         },
       },
+
+      dependsOn: [registryImage],
     });
 
     const noAuthBinding = new DataGoogleIamPolicy(this, "noAuth", {
@@ -86,11 +96,52 @@ class MyStack extends TerraformStack {
     });
 
     new CloudRunServiceIamPolicy(this, "runServiceIamPolicy", {
-      location: region,
-      project: googleProvider.project,
+      location: runService.location,
       service: runService.name,
       policyData: noAuthBinding.policyData,
     });
+
+    const domainMapping = new CloudRunDomainMapping(this, "runDomainMapping", {
+      location: "us-east1",
+      name: "amandahugandkiss.me",
+      metadata: {
+        namespace: project,
+      },
+      spec: {
+        routeName: runService.name,
+      },
+    });
+
+    // If there is a better way to group these more generically, then I'm all for it
+    const ARecordSet = new DnsRecordSet(this, "ARecordSet", {
+      managedZone: "amandahugandkiss-me",
+      name: "amandahugandkiss.me.",
+      type: "A",
+      dependsOn: [domainMapping],
+    });
+
+    ARecordSet.addOverride(
+      "rrdatas",
+      `\${[
+          for record in ${domainMapping.fqn}.status[0].resource_records : record.rrdata if record.type == "A"
+        ]
+      }`
+    );
+
+    const AAAARecordSet = new DnsRecordSet(this, "AAAARecordSet", {
+      managedZone: "amandahugandkiss-me",
+      name: "amandahugandkiss.me.",
+      type: "AAAA",
+      dependsOn: [domainMapping],
+    });
+
+    AAAARecordSet.addOverride(
+      "rrdatas",
+      `\${[
+          for record in ${domainMapping.fqn}.status[0].resource_records : record.rrdata if record.type == "AAAA"
+        ]
+      }`
+    );
   }
 }
 
